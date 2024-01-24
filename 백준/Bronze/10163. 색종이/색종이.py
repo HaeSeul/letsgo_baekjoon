dfe181a2bfce011b08b8d6d30c87d1e7a3c17cf7
haeseul <58204031+haeseul@users.noreply.github.com>
2024-01-24 14:01:20 +0900

    [Bronze I] Title: 색종이, Time: 520 ms, Memory: 116188 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,18 +1,20 @@
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-table = [[0]*1000 for _ in range(1000)]
+table = [[0]*1001 for _ in range(1001)]
 total = [0 for _ in range(N+1)]
 num = 0
+table_size = 0
 for ci, cj, di, dj in arr:
     num += 1
     ni = ci + di - 1
     nj = cj + dj - 1
+    table_size = max(table_size, ni, nj)
     for i in range(di):
         for j in range(dj):
             table[ci+i][cj+j] = num
 
-for tr in range(1000):
-    for tc in range(1000):
+for tr in range(table_size+1):
+    for tc in range(table_size+1):
         if table[tr][tc] == 0:
             continue
         total[table[tr][tc]] += 1
