c0d98fc91c5b5c8de628ab091071605182ccb476
haeseul <58204031+haeseul@users.noreply.github.com>
2024-02-01 15:34:10 +0900

    [Gold V] Title: 토마토, Time: 520 ms, Memory: 352536 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,15 +1,9 @@
 from collections import deque
 import sys
+input = sys.stdin.readline
 sys.setrecursionlimit(10**5)
 
-def isFull(arr):
-    tmp=0
-    for l in arr:
-        tmp+=l.count(1)+l.count(-1)
-    return tmp==N*M
-
-
-def bfs():
+def bfs(cnt):
     while q:
         ci,cj,d=q.popleft()
         for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
@@ -17,24 +11,25 @@ def bfs():
             if 0<=ni<N and 0<=nj<M and not v[ni][nj] and arr[ni][nj]==0:
                 v[ni][nj]=1
                 q.append((ni,nj,d+1))
-    if isFull(v):
+                cnt-=1
+    if cnt>0:
+        return -1
+    else:
         return d-1
-    return -1
-
 
 M,N = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 v = [[0]*M for _ in range(N)]
 q = deque()
+cnt = 0
 
-if isFull(arr):
-    print(0)
-else:
-    for i in range(N):
-        for j in range(M):
-            if arr[i][j]==-1:       # v에 arr의 -1 복사하기
-                v[i][j]=-1
-            if arr[i][j]==1:        # 초기 1의 값 q에 모두 저장
-                q.append((i,j,1))
-                v[i][j] = 1
-    print(bfs())
+for i in range(N):
+    for j in range(M):
+        if arr[i][j]==-1:       # v에 arr의 -1 복사하기
+            v[i][j]=-1
+        elif arr[i][j]==1:        # 초기 1의 값 q에 모두 저장
+            q.append((i,j,1))
+            v[i][j] = 1
+        else:
+            cnt+=1
+print(bfs(cnt))
