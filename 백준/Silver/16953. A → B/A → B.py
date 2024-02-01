32a01e509cba2dc2fcbf2edbd2edd194bdbdf77b
haeseul <58204031+haeseul@users.noreply.github.com>
2024-02-01 11:24:10 +0900

    [Silver II] Title: A → B, Time: 676 ms, Memory: 238100 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -3,18 +3,13 @@ sys.setrecursionlimit(10**5)
 
 def bfs(A):
     q = [(A, 0)]
-    v = []
     while q:
         c, d = q.pop(0)
         if c == B:
             return d+1
-        x,y = c*2, int(str(c)+'1')
-        if c not in v:
-            v.append(c)
-            if x<=B:
-                q.append((x, d+1))
-            if y<=B:
-                q.append((y, d+1))
+        if c < B:
+            q.append((c*2, d+1))
+            q.append((c*10+1, d+1))
     return -1
 
 A,B=map(int, input().split())
