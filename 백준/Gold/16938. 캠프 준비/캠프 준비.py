c6ac1f69c075011d7ed374ce64cc58bf3febe964
haeseul <58204031+haeseul@users.noreply.github.com>
2024-02-06 20:44:37 +0900

    [Gold V] Title: 캠프 준비, Time: 168 ms, Memory: 112340 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.py
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.py
@@ -1,27 +1,36 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(n,s,sm):
+'''
+L <= 2 문제 이상의 합 <= R
+X <= max(level)-min(level)
+
+중복 없이 조합
+가능한 문제 가지수 : N개 중 2개 ~ N개 뽑기
+문제를 하나씩 더해가며 R보다 커지면 back
+전체 다 선택했는데 L보다 작다면 0
+'''
+
+def dfs(n,cnt,sm,mx,mn):
     global ans
-    if L <= sm <= R and max(tmp)-min(tmp) >= X:
-        ans += 1
-    if sm > R:        # sum이 R보다 큰 경우
-        return
+
     if n==N:
-        if sm < L:    # 끝까지 갔는데 L보다 작은 경우
-            return
-        elif max(tmp)-min(tmp) < X:
-            return
+        if cnt>=2 and L <= sm <= R and mx-mn >= X:
+            ans += 1
         return
-    for i in range(s, N):
-        tmp.append(A[i])
-        dfs(n+1, i+1, sm+A[i])
-        tmp.pop()
+
+    if sm > R:  return      # sum이 R보다 큰 경우
+
+    # n번째 포함하지 않는 경우
+    dfs(n+1, cnt, sm, mx, mn)
+    # n번재 포함하는 경우
+    dfs(n+1, cnt+1, sm+A[n], max(mx, A[n]), min(mn, A[n]))
+
 
 N, L, R, X = map(int, input().split())
 A = list(map(int, input().split()))
 ans = 0
 tmp = []
-# 0개 뽑은 상태, start, sum=0
-dfs(0,0,0)
+# 0개 뽑은 상태, cnt, sum, max, min
+dfs(0,0,0,0,10**7)
 print(ans)
