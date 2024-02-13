53c8491b7a7a9b3241c3852ece15a0ac458d618c
haeseul <58204031+haeseul@users.noreply.github.com>
2024-02-13 20:34:41 +0900

    [Gold V] Title: 신기한 소수, Time: 3480 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -0,0 +1,32 @@
+def prime(n):
+    for i in range(2, n-1):
+        if n%i==0:
+            return False
+    return True
+
+def dfs(n,num):
+    if not prime(num):
+        return
+
+    if n==N:
+        print(tmp[-1])
+        return
+
+    for i in odd:
+        x = num*10+i
+        tmp.append(x)
+        dfs(n+1, x)
+        tmp.pop()
+
+
+N=int(input())
+prime_num=[2,3,5,7]
+odd=[1,3,5,7,9]
+tmp=[]
+
+if N==1:
+    for i in prime_num:
+        print(i)
+else:
+    for i in range(len(prime_num)):
+        dfs(1,prime_num[i])
