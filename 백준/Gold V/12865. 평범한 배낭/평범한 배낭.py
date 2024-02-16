1023f9a0c585199ce6c3959466f4fcbf6298318c
haeseul <58204031+haeseul@users.noreply.github.com>
2024-02-16 14:47:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 160 ms, Memory: 110024 KB -BaekjoonHub

--- 백준/Gold V/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold V/12865. 평범한 배낭/평범한 배낭.py
@@ -1,18 +1,10 @@
 # N개 물건, 최대 무게 K
 N,K = map(int, input().split())
-# (무게, 가치) => dp table과 인덱스 맞춰주기 위해 [0] 추가
-a = [0]+[list(map(int, input().split())) for _ in range(N)]
-# 무게(i)에 맞게 각 물건(j)을 넣을 때마다 최대 value 갱신
-dp = [[0]*(N+1) for _ in range(K+1)]
+a = [list(map(int, input().split())) for _ in range(N)]
 
-for i in range(1,K+1):
-    for j in range(1,N+1):
-        # 각 물건(j)
-        w,v = a[j]
-        # 내 무게(w)보다 현재 무게(i)가 크거나 같은 경우에만 가능
-        if w <= i:
-            # 나를 뽑기 전 상태에서 나를 추가했을 때 vs 나를 뽑지 않고 그냥 갈 때
-            dp[i][j] = max(dp[i-w][j-1] + v, dp[i][j-1])
-        else:
-            dp[i][j] = dp[i][j-1]
-print(dp[K][N])
+# 무게(i)별 value만 저장
+dp = [0]*(K+1)
+for w, v in a:
+    for i in range(K, w-1, -1): # 최대 무게부터 해당 물건 무게까지 거꾸로 비교
+        dp[i] = max(dp[i-w] + v, dp[i])
+print(dp[K])
