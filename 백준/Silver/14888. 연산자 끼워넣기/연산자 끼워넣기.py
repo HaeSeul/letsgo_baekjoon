5d1f3020ed87c9bb9619741bdd1bc372f2306462
haeseul <58204031+haeseul@users.noreply.github.com>
2024-02-27 09:13:05 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 236 ms, Memory: 113840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,11 +1,11 @@
 def cal(p):
     ans = A[0]
     for i in range(1,N):
-        if p[i-1]==0:       # 덧셈
+        if p[i-1]==op[0]:       # 덧셈
             ans += A[i]
-        elif p[i-1]==1:     # 뺄셈
+        elif p[i-1]==op[1]:     # 뺄셈
             ans -= A[i]
-        elif p[i-1]==2:     # 곱셈
+        elif p[i-1]==op[2]:     # 곱셈
             ans *= A[i]
         else:               # 나눗셈
             if ans<0:
@@ -17,32 +17,28 @@ def cal(p):
 def permute(n):
     global mn, mx
     if n==N-1:  # 모든 연산자를 다 뽑은 경우 순열 완성
-        # [ 3 ] 각 순열 식일 때의 값 구하기
+        # [ 2 ] 각 순열 식일 때의 값 구하기
         ans = cal(p)
         mn = min(mn, ans)
         mx = max(mx, ans)
         return
-    for i in range(N-1):
-        if not v[i]:
-            v[i]=1
-            p.append(cmd[i])
+    for i in range(4):
+        if v[i] < cmd[i]:
+            v[i]+=1
+            p.append(op[i])
             permute(n+1)
             p.pop()
-            v[i]=0
+            v[i]-=1
 
 N = int(input())
 A = list(map(int, input().split()))
 cmd = list(map(int, input().split()))   # +-*/ : N-1개
-mn,mx = 10**9, -10**9
+op = '+-*/'
+mn,mx = 10**9+1, -10**9-1
 
-# [ 1 ] 연산자를 숫자로 개수만큼 치환하기
-for i in range(4):
-    x = cmd.pop(0)
-    cmd += [i]*x
-
-# [ 2 ] 연산자 순열만들기
+# [ 1 ] 연산자 순열만들기
 p = []
-v = [0]*(N-1)
+v = [0]*4
 permute(0)
 
 print(mx, mn, sep='\n')
