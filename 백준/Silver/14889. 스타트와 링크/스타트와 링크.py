8f9fbc4e3598eb183754ff1df497fa96e37037b2
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-05 21:31:29 +0900

    [Silver I] Title: 스타트와 링크, Time: 788 ms, Memory: 116904 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,37 +1,20 @@
-def start_sm(n, s):
-    global a_sm, tmp1
-    if n == 2:  # 각 팀에서 둘 씩 계산
-        x,y = tmp1[0], tmp1[1]
-        a_sm += arr[x][y] + arr[y][x]
-        return
-    for i in range(s, N//2):
-        tmp1.append(start[i])
-        start_sm(n+1, i+1)
-        tmp1.pop()
-
-def link_sm(n, s):
-    global b_sm, tmp2, link
-    if n == 2:  # 각 팀에서 둘 씩 계산
-        x,y = tmp2[0], tmp2[1]
-        b_sm += arr[x][y] + arr[y][x]
-        return
-    for i in range(s, N//2):
-        tmp2.append(link[i])
-        link_sm(n+1, i+1)
-        tmp2.pop()
-
 def cal(start):
-    global a_sm, b_sm, tmp1, tmp2, link
     a_sm, b_sm = 0,0
 
+    # 팀 분배
     link = []
-    # start, link 팀 분배
     for x in range(N):
         if x not in start:
             link.append(x)
-    tmp1, tmp2 = [], []
-    start_sm(0,0)
-    link_sm(0,0)
+    
+    # 팀 별 능력치 합 구하기
+    for i in start:
+        for j in start:
+            if i!=j:    a_sm += arr[i][j]
+    for i in link:
+        for j in link:
+            if i!=j:    b_sm += arr[i][j]
+
     return abs(a_sm - b_sm)
 
 
