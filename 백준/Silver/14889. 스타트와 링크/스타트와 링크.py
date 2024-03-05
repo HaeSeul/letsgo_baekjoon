892c17e2f869d25f2f467237285548eebee595dd
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-05 21:53:04 +0900

    [Silver I] Title: 스타트와 링크, Time: 784 ms, Memory: 118408 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,27 +1,22 @@
-def cal(start):
-    a_sm, b_sm = 0,0
-
-    # 팀 분배
-    link = []
-    for x in range(N):
-        if x not in start:
-            link.append(x)
-    
+def cal(team):
+    sm = 0
     # 팀 별 능력치 합 구하기
-    for i in start:
-        for j in start:
-            if i!=j:    a_sm += arr[i][j]
-    for i in link:
-        for j in link:
-            if i!=j:    b_sm += arr[i][j]
-
-    return abs(a_sm - b_sm)
+    for i in team:
+        for j in team:
+            if i!=j:    sm += arr[i][j]
+    return sm
 
 
 def split(n,s):
     global mn
     if n == N//2:
-        mn = min(mn, cal(start))
+        # 팀 분배
+        link = []
+        for x in range(N):
+            if x not in start:
+                link.append(x)
+        # 각 팀의 점수 합의 최소
+        mn = min(mn, abs(cal(start) - cal(link)))
         return
     for i in range(s, N):
         start.append(i)
@@ -30,7 +25,7 @@ def split(n,s):
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-mn = 21e8
+mn = N*N*100
 # N명을 2개 팀으로 나누기 : N개 중 N/2개 뽑기
 start = []
 split(0,0)
