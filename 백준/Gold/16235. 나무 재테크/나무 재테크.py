3e899e2c72914ec5398b920be41e823058dcbc17
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-07 11:42:50 +0900

    [Gold III] Title: 나무 재테크, Time: 652 ms, Memory: 183952 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -4,7 +4,8 @@ from collections import deque
 
 # N*N, M개 나무 심기, K년 이후
 N,M,K = map(int, input().split())
-arr = [[[5, deque()] for _ in range(N)] for _ in range(N)]
+arr = [[5]*N for _ in range(N)]
+age = [[deque() for _ in range(N)] for _ in range(N)]
 dir = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 
 # 매 겨울마다 추가되는 양분
@@ -13,7 +14,7 @@ plus = [list(map(int, input().split())) for _ in range(N)]
 # 초기 나무 심기
 for _ in range(M):
     ti,tj,ta = map(int, input().split())    # i-1,j-1 해주기
-    arr[ti-1][tj-1][1].append(ta)
+    age[ti-1][tj-1].append(ta)
 
 # K년 반복
 for _ in range(K):
@@ -22,22 +23,22 @@ for _ in range(K):
     # 1. 양분먹거나 죽기
     for i in range(N):
         for j in range(N):
-            if not arr[i][j][1]: continue # 나무가 없는 경우
+            if not age[i][j]: continue  # 나무가 없는 경우
 
             dead = 0
-            trees = arr[i][j][1]
+            trees = age[i][j]
 
             for n in range(len(trees)):
                 tree = trees.popleft()
-                if arr[i][j][0] >= tree:        # 양분이 충분하다면
-                    arr[i][j][0] -= tree            # 나이만큼 양분 추가
-                    arr[i][j][1].append(tree+1)     # 1살 더 먹기
-                    if (tree+1)%5 == 0:     # 5의 배수라면 가을에 번식
+                if arr[i][j] >= tree:           # 양분이 충분하다면
+                    arr[i][j] -= tree           # 나이만큼 양분 추가
+                    age[i][j].append(tree+1)    # 1살 더 먹기
+                    if (tree+1)%5 == 0:         # 5의 배수라면 가을에 번식
                         grow[i][j] += 1
-                else:               # 양분이 부족하다면 죽음
+                else:                           # 양분이 부족하다면 죽음
                     dead += (tree // 2)
 
-            arr[i][j][0] += dead    # 죽은만큼 추가
+            arr[i][j] += dead    # 죽은만큼 추가
 
     # 2. 나무번식 + 양분추가
     for i in range(N):
@@ -48,13 +49,13 @@ for _ in range(K):
                     ni, nj = i + di, j + dj
                     if not (0 <= ni < N and 0 <= nj < N): continue
                     for _ in range(cnt):    # 자라야하는 만큼 추가
-                        arr[ni][nj][1].appendleft(1)
-            arr[i][j][0] += plus[i][j]
+                        age[ni][nj].appendleft(1)
+            arr[i][j] += plus[i][j]
 
 # 살아남은 나무 개수
 ans = 0
 for i in range(N):
     for j in range(N):
-        if not arr[i][j][1]: continue
-        ans += len(arr[i][j][1])
+        if not age[i][j]: continue
+        ans += len(age[i][j])
 print(ans)
