dc972f39836c721af3e5da9dbe6a95f84b810339
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-07 10:53:46 +0900

    [Gold III] Title: 나무 재테크, Time: 852 ms, Memory: 189880 KB -BaekjoonHub

---
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -0,0 +1,61 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+# N*N, M개 나무 심기, K년 이후
+N,M,K = map(int, input().split())
+arr = [[[5, deque()] for _ in range(N)] for _ in range(N)]
+dir = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+
+# 매 겨울마다 추가되는 양분
+plus = [list(map(int, input().split())) for _ in range(N)]
+
+# 초기 나무 심기
+for _ in range(M):
+    ti,tj,ta = map(int, input().split())    # i-1,j-1 해주기
+    arr[ti-1][tj-1][1].append(ta)
+
+# K년 반복
+for _ in range(K):
+    grow = []
+
+    # 1. 양분먹거나 죽기
+    for i in range(N):
+        for j in range(N):
+            if not arr[i][j][1]: continue # 나무가 없는 경우
+
+            dead = 0
+            trees = arr[i][j][1]
+
+            for n in range(len(trees)):
+                tree = trees.popleft()
+                if arr[i][j][0] >= tree:        # 양분이 충분하다면
+                    arr[i][j][0] -= tree            # 나이만큼 양분 추가
+                    arr[i][j][1].append(tree+1)     # 1살 더 먹기
+                    if (tree+1)%5 == 0:     # 5의 배수라면 가을에 번식
+                        grow.append((i,j))
+                else:               # 양분이 부족하다면 죽음
+                    dead += (tree // 2)
+
+            arr[i][j][0] += dead    # 죽은만큼 추가
+
+    # 2. 나무번식
+    if grow:
+        for i,j in grow:
+            for di,dj in dir:
+                ni,nj = i+di, j+dj
+                if not (0<=ni<N and 0<=nj<N): continue
+                arr[ni][nj][1].appendleft(1)
+
+    # 3. 양분추가
+    for i in range(N):
+        for j in range(N):
+            arr[i][j][0] += plus[i][j]
+
+# 살아남은 나무 개수
+ans = 0
+for i in range(N):
+    for j in range(N):
+        if not arr[i][j][1]: continue
+        ans += len(arr[i][j][1])
+print(ans)
