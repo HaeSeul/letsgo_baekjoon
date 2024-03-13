6f00fa00de5489fcfb8be7202bbbfffad214180f
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-13 11:29:23 +0900

    [Gold III] Title: 아기 상어, Time: 216 ms, Memory: 114672 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -2,14 +2,16 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-def cal(i,j,fi,fj):
-    v = [[0]*N for _ in range(N)]
+def find(i,j):
+    eat = []
     v[i][j] = 0
     q = deque([(i,j)])
     while q:
         ci,cj = q.popleft()
-        if (ci,cj)==(fi,fj):
+        
+        if 0< arr[ci][cj] < size:
             eat.append([ci,cj,v[ci][cj]])   # [먹이위치, 거리]
+        
         for di,dj in ((-1,0),(0,1),(1,0),(0,-1)):
             ni,nj = ci+di, cj+dj
             if not (0<=ni<N and 0<=nj<N): continue
@@ -17,44 +19,36 @@ def cal(i,j,fi,fj):
             if arr[ni][nj] > size: continue
             v[ni][nj] = v[ci][cj]+1
             q.append((ni,nj))
-
-
-def find():
-    fish = []   # 가능한 물고기들
-    for i in range(N):
-        for j in range(N):
-            if 0 < arr[i][j] < size:
-                fish.append((i, j))
-    return fish
+    
+    return eat
 
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-# 상어위치, 2보다 작은 물고기위치
+# 상어 초기화
 ci,cj,size = 0,0,2
 for i in range(N):
     for j in range(N):
         if not arr[i][j]: continue
         if arr[i][j]==9:
             ci,cj = i,j
-            arr[i][j] = 0
+            arr[i][j] = 0   # 상어위치 0으로 바꿔주기
 
 time = 0
 cnt = 0         # 먹은 물고기 수
 
 while True:
-    fish = find()       # 가능한 물고기 찾기
+    v = [[0]*N for _ in range(N)]
 
-    eat = []
-    for fi,fj in fish:  # 현재 위치에서 물고기까지 거리 계산
-        cal(ci,cj,fi,fj)
+    eat = find(ci,cj)       # 가능한 물고기 찾기
 
-    if not eat: break
+    if not eat: break       # 먹을 게 없으면 종료
 
-    eat.sort(key=lambda x:(x[2],x[0],x[1]))    # 거리,행,열 순 정렬
+    if len(eat) > 1:        # 거리,행,열 순 정렬
+        eat.sort(key=lambda x:(x[2],x[0],x[1]))
 
-    ti, tj, td = eat[0][0], eat[0][1], eat[0][2]
+    ti, tj, td = eat[0]
     ci, cj = ti, tj     # 상어이동
     time += td          # 거리만큼 시간 ++
     arr[ti][tj] = 0     # 물고기먹기
