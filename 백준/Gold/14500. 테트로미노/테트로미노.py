0603bc6fa54b2fb67b940f294fd5df0cc2ab946b
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-29 21:32:02 +0900

    [Gold IV] Title: 테트로미노, Time: 216 ms, Memory: 115072 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,27 +1,31 @@
+def dfs(sm,lst):
+    global mx
+    # 지금 sum에 지금 있는 lst*MAX 를 해도 mx보다 작으면 가지치기
+    if sm + (4-len(lst))*MAX < mx: return
+
+    if len(lst)==4:
+        mx = max(mx, sm)
+        return
+
+    for ci,cj in lst:
+        for d in range(4):
+            ni,nj = ci+dir[d][0], cj+dir[d][1]
+            if not (0<=ni<N and 0<=nj<M): continue
+            if v[ni][nj]: continue
+            v[ni][nj] = 1
+            dfs(sm+arr[ni][nj], lst+[(ni,nj)])
+            v[ni][nj] = 0
+
 N,M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+dir = ((-1,0),(0,1),(1,0),(0,-1))
 
+MAX = max(map(max, arr))
 mx = -1
-
-shapes = [[[1,1],[1,1]]]
-r1 = [[[1,1,1,1]], [[1,0],[1,1],[0,1]], [[0,1],[1,1],[1,0]]]
-r4 = [[[1,0],[1,0],[1,1]], [[0,1],[0,1],[1,1]], [[1,1,1],[0,1,0]]]
-for r in r1:
-    shapes.append(r)
-    shapes.append(list(map(list, zip(*r[::-1]))))
-for r in r4:
-    for _ in range(4):
-        r = list(map(list, zip(*r[::-1])))
-        shapes.append(r)
-
-for shape in shapes:
-    n,m = len(shape), len(shape[0])
-    for i in range(N-n+1):
-        for j in range(M-m+1):
-            sm = 0
-            for r in range(n):
-                for c in range(m):
-                    if shape[r][c]:
-                        sm += arr[i+r][j+c]
-            mx = max(mx, sm)
+v = [[0]*M for _ in range(N)]
+for i in range(N):
+    for j in range(M):
+        v[i][j] = 1
+        dfs(arr[i][j],[(i,j)])
+        v[i][j] = 0
 print(mx)
