7ce33eeec7a0d9385033d08ad2832a4f021ac9a8
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-29 19:24:18 +0900

    [Gold II] Title: 2048 (Easy), Time: 252 ms, Memory: 116288 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,78 +1,72 @@
-def right(arr):
-    # 오른쪽으로 옮기기
-    for i in range(N):
-        turn = [0] * N
+from collections import deque
 
-        # 행의 뒤에서 두번째 것부터 시작
-        for j in range(len(arr) - 2, -1, -1):
-            if arr[i][j] == 0: continue
-            idx = j
-
-            while True:
-                if idx > N - 2: break  # 맨 끝에 도달하면 끝
-
-                # 내 옆이 나와 같고 지금 턴에서 바꾸지 않았다면 합치기
-                if arr[i][idx + 1] and arr[i][idx] == arr[i][idx + 1] and not turn[idx + 1]:
-                    turn[idx + 1] = 1
-                    arr[i][idx + 1] *= 2
-                    arr[i][idx] = 0
-                    break
-                # 내 뒤에 다른 숫자가 있다면 끝
-                if arr[i][idx + 1]: break
-
-                # 숫자 옮기기
-                arr[i][idx + 1] = arr[i][idx]
-                arr[i][idx] = 0
-                idx += 1
+def push(arr):
+    # 왼쪽으로 밀기
+    for r in range(N):
+        row = deque()
+        for c in range(N):
+            if arr[r][c] != 0:  # 0 제외하고 deque에 담기
+                row.append(arr[r][c])
+        lst = []
+        while row:
+            if len(row) >= 2:
+                if row[0] == row[1]:
+                    lst.append(row.popleft()*2)
+                    row.popleft()
+                else:
+                    lst.append(row.popleft())
+            else:
+                lst.append(row.popleft())
+        lst += [0] * (N-len(lst))
+        arr[r] = lst
     return arr
 
-def up(arr):
-    # 위로 올리기
+# 위로밀기 (반시계 90 -> 시계 90)
+def push_up(arr):
+    arr = list(map(list, zip(*arr)))[::-1]
+    arr = push(arr)
     arr = list(map(list, zip(*arr[::-1])))
-    arr = right(arr)
-    for _ in range(3):
-        arr = list(map(list, zip(*arr[::-1])))
     return arr
 
-def down(arr):
-    arr = list(map(list, zip(*arr)))
-    arr = right(arr)
-    arr = list(map(list, zip(*arr)))
+# 아래로밀기 (시계 90 -> 반시계 90)
+def push_down(arr):
+    arr = list(map(list, zip(*arr[::-1])))
+    arr = push(arr)
+    arr = list(map(list, zip(*arr)))[::-1]
     return arr
 
-def left(arr):
-    for i in range(N):
-        arr[i] = arr[i][::-1]
-    arr = right(arr)
-    for i in range(N):
-        arr[i] = arr[i][::-1]
+# 오른쪽으로밀기 (전치)
+def push_right(arr):
+    arr = [l[::-1] for l in arr]
+    arr = push(arr)
+    arr = [l[::-1] for l in arr]
     return arr
-
 
 def dfs(n, arr):
     global mx
-    if n==5:    # 5번 반복하면 끝
-        for l in arr:
-            mx = max(mx, *l)
+    if n==5:
+        mx = max(mx, max(map(max, arr)))
         return
 
-    new_arr = [list(l) for l in arr[::]]
-
+    n_arr = [l[::] for l in arr]
     for d in range(4):
-        # 배열 돌리기
-        if d==0:
-            new_arr = right(new_arr)
-        elif d==1:
-            new_arr = down(new_arr)
-        elif d==2:
-            new_arr = left(new_arr)
-        else:
-            new_arr = up(new_arr)
-        dfs(n+1, new_arr)
-        new_arr = [list(l) for l in arr[::]]
+        if d==0:    # 왼
+            n_arr = push(n_arr)
+        elif d==1:  # 아
+            n_arr = push_down(n_arr)
+        elif d==2:  # 오
+            n_arr = push_right(n_arr)
+        else:       # 위
+            n_arr = push_up(n_arr)
+        dfs(n + 1, n_arr)
+        n_arr = [l[::] for l in arr]
+
+
+dir = ((0,-1),(-1,0),(0,1),(-1,0))  # 왼아오위
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-mx = 0
-dfs(0, arr)
+mx = -1
+
+dfs(0,arr)
 print(mx)
