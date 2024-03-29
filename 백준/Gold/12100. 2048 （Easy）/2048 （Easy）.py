34db9e77eef8c06ca6724e7101f05bc571efb363
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-29 21:05:16 +0900

    [Gold II] Title: 2048 (Easy), Time: 204 ms, Memory: 114760 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -21,27 +21,6 @@ def push(arr):
         arr[r] = lst
     return arr
 
-# 위로밀기 (반시계 90 -> 시계 90)
-def push_up(arr):
-    arr = list(map(list, zip(*arr)))[::-1]
-    arr = push(arr)
-    arr = list(map(list, zip(*arr[::-1])))
-    return arr
-
-# 아래로밀기 (시계 90 -> 반시계 90)
-def push_down(arr):
-    arr = list(map(list, zip(*arr[::-1])))
-    arr = push(arr)
-    arr = list(map(list, zip(*arr)))[::-1]
-    return arr
-
-# 오른쪽으로밀기 (전치)
-def push_right(arr):
-    arr = [l[::-1] for l in arr]
-    arr = push(arr)
-    arr = [l[::-1] for l in arr]
-    return arr
-
 def dfs(n, arr):
     global mx
     if n==5:
@@ -49,20 +28,21 @@ def dfs(n, arr):
         return
 
     n_arr = [l[::] for l in arr]
-    for d in range(4):
-        if d==0:    # 왼
-            n_arr = push(n_arr)
-        elif d==1:  # 아
-            n_arr = push_down(n_arr)
-        elif d==2:  # 오
-            n_arr = push_right(n_arr)
-        else:       # 위
-            n_arr = push_up(n_arr)
-        dfs(n + 1, n_arr)
-        n_arr = [l[::] for l in arr]
+    n_arr = push(n_arr)
+    dfs(n + 1, n_arr)
 
+    n_arr = list(map(list, zip(*arr)))[::-1]
+    n_arr = push(n_arr)
+    dfs(n + 1, n_arr)
 
-dir = ((0,-1),(-1,0),(0,1),(-1,0))  # 왼아오위
+    n_arr = list(map(list, zip(*arr[::-1])))
+    n_arr = push(n_arr)
+    dfs(n + 1, n_arr)
+
+    n_arr = [l[::-1] for l in arr]
+    n_arr = push(n_arr)
+    dfs(n + 1, n_arr)
+
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
