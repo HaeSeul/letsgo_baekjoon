b28062a62be0d79db55b1d58942109136964eaab
haeseul <58204031+haeseul@users.noreply.github.com>
2024-03-29 19:47:29 +0900

    [Gold IV] Title: 테트로미노, Time: 488 ms, Memory: 112744 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,58 +1,27 @@
-def flip(origin, R, C):
-    new = [[0] * C for _ in range(R)]
-    for i in range(R):
-        for j in range(C):
-            new[i][j] = origin[i][C-1-j]
-    return new
-
-
-def rotate90(origin, R, C):
-    R,C = C,R
-    new = [[0]*C for _ in range(R)]
-    for i in range(R):
-        for j in range(C):
-            new[i][j] = origin[C-1-j][i]
-    return new, R, C
-
-T1 = [[1,1,1,1]]
-T2 = [[1,0],[1,0],[1,1]]
-T3 = [[1,0],[1,1],[0,1]]
-T4 = [[1,1,1],[0,1,0]]
-T5 = [[1,1],[1,1]]
-T = [T1,T2,T3,T4,T5]
-
-def check(t, r, c, R, C):
-    cnt = 0
-    for i in range(R):
-        for j in range(C):
-            if t[i][j] == 1:
-                cnt += arr[i+r][j+c]
-    return cnt
-
 N,M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-mx = 0
 
-for t in T:
-    R,C = len(t), len(t[0])
+mx = -1
 
-    # 기존 모형에서 회전 4회
+shapes = [[[1,1],[1,1]]]
+r1 = [[[1,1,1,1]], [[1,0],[1,1],[0,1]], [[0,1],[1,1],[1,0]]]
+r4 = [[[1,0],[1,0],[1,1]], [[0,1],[0,1],[1,1]], [[1,1,1],[0,1,0]]]
+for r in r1:
+    shapes.append(r)
+    shapes.append(list(map(list, zip(*r[::-1]))))
+for r in r4:
     for _ in range(4):
-        t, R, C = rotate90(t, R, C)
-        for r in range(N-R+1):
-            for c in range(M-C+1):
-                tmp = check(t, r, c, R, C)
-                mx = max(mx, tmp)
+        r = list(map(list, zip(*r[::-1])))
+        shapes.append(r)
 
-    # flip
-    t = flip(t, R, C)
-
-    # flip 모형에서 회전 4회
-    for _ in range(4):
-        t, R, C = rotate90(t, R, C)
-        for r in range(N-R+1):
-            for c in range(M-C+1):
-                tmp = check(t, r, c, R, C)
-                mx = max(mx, tmp)
-
+for shape in shapes:
+    n,m = len(shape), len(shape[0])
+    for i in range(N-n+1):
+        for j in range(M-m+1):
+            sm = 0
+            for r in range(n):
+                for c in range(m):
+                    if shape[r][c]:
+                        sm += arr[i+r][j+c]
+            mx = max(mx, sm)
 print(mx)
