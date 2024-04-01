f625f75a9053c0eef5779f50cf760debd29eabf4
haeseul <58204031+haeseul@users.noreply.github.com>
2024-04-01 09:50:48 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 136 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -1,104 +1,61 @@
-def tilt(d, i, j, color):       # d로 기울였을 때 가능한 위치 (0:red, 1:blue)
-    dist = 0
-    ni,nj = i+dir[d][0], j+dir[d][1]
-    while arr[ni][nj]=='.' or arr[ni][nj]=='O':
-        if arr[ni][nj]=='O':
-            if color == 0:      # red가 구멍 만난 경우
-                return dist + 1
-            else:               # blue가 구멍 만난 경우
-                return -1
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+def tilt(ni,nj,d):
+    while arr[ni+dir[d][0]][nj+dir[d][1]] != '#' and arr[ni][nj] != 'O':
         ni,nj = ni+dir[d][0], nj+dir[d][1]
-        dist += 1
-    # (ri+dir[d][0]*dist, rj+dir[d][1]*dist)
-    return dist
+    return ni,nj
 
+def bfs(ri,rj,bi,bj):
+    v = set()
+    v.add((ri,rj,bi,bj))
+    q = deque([(ri,rj,bi,bj)])
+    cnt = 1
+    while q:
+        for _ in range(len(q)):
+            ri, rj, bi, bj = q.popleft()
 
-def dfs(n, ri, rj, bi, bj, now_d):  # r/b의 현재위치, 회전방향
-    global ans
-    # 종료조건
-    if arr[ri][rj] == 'O':
-        ans.add(n)
-        return
-    if n == 10:     # 10까지 왔는데 빨간 구슬이 못 간 경우
-        if arr[ri][rj] != 'O':
-            ans.add(21e8)
-        return
+            # 종료조건
+            if cnt > 10:
+                print(-1)
+                return
 
+            for d in range(4):
+                nri, nrj = tilt(ri, rj, d)
+                nbi, nbj = tilt(bi, bj, d)
+                if arr[nbi][nbj] == 'O': continue # 파랑 -> 구멍으로 가면 패스
+                if arr[nri][nrj] == 'O':          # 빨강 -> 구멍으로 가면 끝!
+                    print(cnt)
+                    return
 
-    for d in range(4):
-        # 현재 방향과 다른 방향만 탐색
-        if d == now_d: continue
+                # 움직인 곳이 같은 경우 더 많이 온 곳을 한 칸 뒤로
+                if (nri, nrj) == (nbi, nbj):
+                    if abs(ri-nri)+abs(rj-nrj) > abs(bi-nbi)+abs(bj-nbj):
+                        nri, nrj = nri-dir[d][0], nrj-dir[d][1]
+                    else:
+                        nbi, nbj = nbi-dir[d][0], nbj-dir[d][1]
 
-        # b의 방향으로 구슬이 없는 경우만 탐색
-        b_dist = tilt(d, bi, bj, 1)
-        if b_dist == -1: continue
-
-        b_ni, b_nj = bi + dir[d][0] * b_dist, bj + dir[d][1] * b_dist
-        r_dist = tilt(d, ri, rj, 0)
-        r_ni, r_nj = ri + dir[d][0] * r_dist, rj + dir[d][1] * r_dist
-
-        if d == 0:
-            # 0) 오른쪽 기울이기
-            if ri == bi and r_nj == b_nj:  # 같은 행에 같은 지점인데
-                if rj < bj:  # blue가 오른쪽이라면 red를 한 칸 왼쪽으로
-                    dfs(n + 1, r_ni, r_nj - 1, b_ni, b_nj, 0)
-                else:  # red가 더 오른쪽이라면 blue를 한 칸 왼쪽으로
-                    dfs(n + 1, r_ni, r_nj, b_ni, b_nj - 1, 0)
-            else:
-                dfs(n + 1, r_ni, r_nj, b_ni, b_nj, 0)
-
-        elif d == 1:
-            # 1) 아래쪽 기울이기
-            if rj == bj and r_ni == b_ni:  # 같은 열에
-                if ri < bi:  # blue가 아래라면 red를 한 칸 위으로
-                    dfs(n + 1, r_ni - 1, r_nj, b_ni, b_nj, 1)
-                else:  # red가 더 아래라면 blue를 한 칸 위로
-                    dfs(n + 1, r_ni, r_nj, b_ni - 1, b_nj, 1)
-            else:
-                dfs(n + 1, r_ni, r_nj, b_ni, b_nj, 1)
-
-        elif d == 2:
-            # 2) 왼쪽으로 기울이기
-            if ri == bi and r_nj == b_nj:  # 같은 행에
-                if rj < bj:  # red가 왼쪽이라면 blue를 한 칸 오른쪽으로
-                    dfs(n + 1, r_ni, r_nj, b_ni, b_nj + 1, 2)
-                else:  # blue가 더 왼쪽이라면 red를 한 칸 오른쪽으로
-                    dfs(n + 1, r_ni, r_nj + 1, b_ni, b_nj, 2)
-            else:
-                dfs(n + 1, r_ni, r_nj, b_ni, b_nj, 2)
-
-        else:
-            # 3) 위로 기울이기
-            if rj == bj and r_ni == b_ni:  # 같은 열에
-                if ri < bi:  # red가 더 위라면 blue를 한 칸 아래로
-                    dfs(n + 1, r_ni, r_nj, b_ni + 1, b_nj, 3)
-                else:  # blue가 위라면 red를 한 칸 아래로
-                    dfs(n + 1, r_ni + 1, r_nj, b_ni, b_nj, 3)
-            else:
-                dfs(n + 1, r_ni, r_nj, b_ni, b_nj, 3)
-
-
+                if (nri, nrj, nbi, nbj) in v: continue  # 이미 왔던 곳 패스
+                v.add(((nri, nrj, nbi, nbj)))
+                q.append(((nri, nrj, nbi, nbj)))
+        cnt += 1
+    # 못 가는 경우
+    print(-1)
 
 N,M = map(int, input().split())
-arr = [list(input()) for _ in range(N)]
-dir = ((0,1),(1,0),(0,-1),(-1,0))       # 우 하 좌 상
-ans = set()
-end = 0
+arr = [list() for _ in range(N)]
+dir = ((-1,0),(0,1),(1,0),(0,-1))
 
 ri,rj,bi,bj = 0,0,0,0
 for i in range(N):
+    a = list(input())
     for j in range(M):
-        if arr[i][j] == 'R':
+        arr[i].append(a[j])
+        if a[j] == 'R':
             ri,rj = i,j
-            arr[i][j] = '.'
-        elif arr[i][j] == 'B':
+        if a[j] == 'B':
             bi,bj = i,j
-            arr[i][j] = '.'
 
-dfs(0,ri,rj,bi,bj,-1)
-
-mn = min(ans)
-if mn == 21e8:
-    print(-1)
-else:
-    print(mn)
+bfs(ri,rj,bi,bj)
