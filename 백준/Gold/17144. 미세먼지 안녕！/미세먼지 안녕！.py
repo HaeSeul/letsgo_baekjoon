f35e117f9764f7e670b2db9aa4346e53bee86622
haeseul <58204031+haeseul@users.noreply.github.com>
2024-04-01 21:08:49 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 336 ms, Memory: 111496 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,36 +1,19 @@
-def upper():
-    # 위쪽 청정기 돌리기
-    for i in range(a-1, 0, -1):
-        arr[i][0] = arr[i-1][0]
-    arr[0] = arr[0][1:] + [arr[1][M-1]]
-    for i in range(1, a):
-        arr[i][M-1] = arr[i+1][M-1]
-    arr[a][1:] = [0] + arr[a][1:M-1]
-
-def lower():
-    # 아래쪽 청정기
-    for i in range(b+1, N-1):
-        arr[i][0] = arr[i+1][0]
-    arr[N-1] = arr[N-1][1:] + [arr[N-2][M-1]]
-    for i in range(N-1, b, -1):
-        arr[i][M-1] = arr[i-1][M-1]
-    arr[b][1:] = [0] + arr[b][1:M-1]
-
-
 N,M,T = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+air = [[(0,1),(-1,0),(0,-1),(1,0)], [(0,1),(1,0),(0,-1),(-1,0)]]
 
 # 청정기 위치 찾기
-a, b = 0,0
+m = []
 for i in range(N):
     if arr[i][0] == -1:
-        a,b = i, i+1
+        m.append(i)
+        m.append(i+1)
         break
 
 for _ in range(T):
     # 미세먼지 확산
     new = [[0]*M for _ in range(N)]
-    new[a][0] = new[b][0] = -1
+    new[m[0]][0] = new[m[1]][0] = -1
     for i in range(N):
         for j in range(M):
             if not arr[i][j] or arr[i][j] == -1: continue
@@ -45,12 +28,23 @@ for _ in range(T):
             new[i][j] += arr[i][j] - spread * cnt
     arr = new
 
-    upper()
-    lower()
+    # 청정기 (위/아래)
+    for x in range(2):
+        si,sj,sd = m[x],1,0     # 기계에서 나오는 것부터 시작
 
-ans = 0
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] > 0:
-            ans += arr[i][j]
-print(ans)
+        tmp = arr[si][sj]
+        arr[si][sj] = 0     # 새로운 바람
+
+        while True:
+            ni,nj = si+air[x][sd][0], sj+air[x][sd][1]  # 기계 위/아래의 방향
+
+            if not (0<=ni<N and 0<=nj<M):   # 벗어나면 방향 바꾼 것으로 갱신
+                sd += 1
+                continue
+            if arr[ni][nj] == -1:   # 청정기 만날때까지 반복
+                break
+
+            arr[ni][nj], tmp = tmp, arr[ni][nj] # 계속 옆의 것과 교체
+            si,sj = ni,nj
+
+print(sum(map(sum, arr)) + 2)
