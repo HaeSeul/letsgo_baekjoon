8765e974ff50646d532f3ef77bd8b2bfa0170844
haeseul <58204031+haeseul@users.noreply.github.com>
2024-04-02 21:07:08 +0900

    [Gold II] Title: 원판 돌리기, Time: 212 ms, Memory: 112252 KB -BaekjoonHub

--- 백준/Gold/17822. 원판 돌리기/원판 돌리기.py
+++ 백준/Gold/17822. 원판 돌리기/원판 돌리기.py
@@ -1,62 +1,41 @@
-from collections import deque
+N,M,T = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-def get():
+for _ in range(T):
+    X,D,K = map(int, input().split())
+
+    for i in range(N):
+        if (i+1)%X != 0: continue   # X의 배수인 원만
+
+        for _ in range(K):
+            if D == 0:
+                arr[i] = [arr[i][-1]] + arr[i][:-1]
+            else:
+                arr[i] = arr[i][1:] + [arr[i][0]]
+
+    nothing = True
+    new = [l[:] for l in arr]
     sm, cnt = 0, 0
-    for i in range(1, N+1):
+    for i in range(N):
         for j in range(M):
             if not arr[i][j]: continue
             sm += arr[i][j]
             cnt += 1
-    return sm, cnt
+            for di,dj in ((-1,0),(0,1),(1,0),(0,-1)):
+                ni,nj = i+di, (j+dj)%M
+                if not (0<=ni<N): continue
+                if not arr[ni][nj]: continue
+                if arr[i][j] == arr[ni][nj]:
+                    new[i][j], new[ni][nj] = 0,0
+                    nothing = False
+    if nothing and cnt:
+        mean = sm/cnt
+        for i in range(N):
+            for j in range(M):
+                if not new[i][j]: continue
+                if new[i][j] > mean: new[i][j] -= 1
+                elif new[i][j] < mean: new[i][j] += 1
 
-N,M,T = map(int, input().split())
-# arr 처음/마지막에 padding 추가
-arr = [deque([0])*M] + [deque(map(int, input().split())) for _ in range(N)] + [deque([0])*M]
-dir = ((-1,0),(0,1),(1,0),(0,-1))
+    arr = new
 
-for t in range(T):
-    X,D,K = map(int, input().split())
-
-    # 원판 회전
-    for i in range(1, N+1):
-        if i % X != 0: continue  # X의 배수인 행만
-
-        circle = arr[i]
-        if D == 0:  # 시계방향 회전
-            for _ in range(K):
-                circle.appendleft(circle.pop())
-        else:       # 반시계 회전
-            for _ in range(K):
-                circle.append(circle.popleft())
-
-    # 인접한 같은 수 찾기
-    v = [[0]*M for _ in range(N+2)]
-    same = set()
-    for i in range(1, N+1):
-        for j in range(M):
-            if arr[i][j] == 0: continue
-            for di, dj in ((-1,0),(0,1),(1,0),(0,-1)):
-                ni, nj = i+di, (j+dj)%M
-                if v[ni][nj]: continue
-                if arr[ni][nj] == 0: continue
-                if arr[ni][nj] == arr[i][j]:
-                    same.add((i,j))
-                    same.add((ni,nj))
-                    v[i][j] = 1     # 나만 일단 방문처리
-
-    # 같은 수가 있다면 0 처리
-    if same:
-        for i,j in same:
-            arr[i][j] = 0
-    else:
-        sm, cnt = get()
-        if cnt > 0:
-            avg = sm / cnt
-            for i in range(1, N+1):
-                for j in range(M):
-                    if arr[i][j] == 0: continue
-                    if arr[i][j] > avg:     arr[i][j] -= 1
-                    elif arr[i][j] < avg:   arr[i][j] += 1
-
-sm, cnt = get()
-print(sm)
+print(sum(map(sum, arr)))
