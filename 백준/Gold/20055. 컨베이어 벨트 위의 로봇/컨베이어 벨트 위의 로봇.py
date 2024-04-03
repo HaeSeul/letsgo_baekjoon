bfa5f28ef2643d8ff2dedf407af559969e66af25
haeseul <58204031+haeseul@users.noreply.github.com>
2024-04-03 09:38:27 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 348 ms, Memory: 111456 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,44 +1,33 @@
-from collections import deque
+N,K = map(int, input().split())
+belt = list(map(int, input().split()))
+robot = [0 for _ in range(2*N)]
+turn = 1
 
-def solve():
-    global turn,num
-    while True:
-        # 벨트 회전
-        turn += 1
-        belt.appendleft(belt.pop())
-        # 내리는 곳에 있는 로봇 내려주기
-        if belt[N - 1][1] == 1:
-            belt[N - 1][1] = 0
+while True:
+    # 1. 벨트 & 로봇회전
+    belt = [belt[-1]] + belt[:-1]
+    robot = [robot[-1]] + robot[:-1]
+    if robot[N-1]:   # 로봇 내리기
+        robot[N-1] = 0
 
-        # 올리는 곳 내구도>0 & 로봇이 없을 때
-        if belt[0][0] > 0 and not belt[0][1]:
-            belt[0][1] = 1  # 로봇올리기
-            belt[0][0] -= 1  # 내구도--
-            if belt[0][0] == 0:
-                num += 1
-                if num >= K:
-                    return turn
+    # 2. 로봇이동
+    for i in range(N-2, -1, -1):
+        if not robot[i]: continue
+        if robot[i+1]: continue     # 다른로봇
+        if not belt[i+1]: continue  # 내구도 없음
+        robot[i], robot[i+1] = robot[i+1], robot[i]
+        belt[i+1] -= 1
+    if robot[N-1]:   # 로봇 내리기
+        robot[N-1] = 0
 
-        # 먼저 올라간 로봇부터 이동
-        for i in range(N - 2, 0, -1):
-            # 현재 로봇이 있고 옆에 내구도>0 & 로봇이 없을 때
-            if belt[i][1] and belt[i + 1][0] > 0 and not belt[i + 1][1]:
-                belt[i][1] = 0  # 로봇옮기기
-                if i + 1 != N - 1:  # 내리는 위치가 아닐 때만 옮겨주기
-                    belt[i + 1][1] = 1
-                belt[i + 1][0] -= 1  # 내구도--
-                if belt[i + 1][0] == 0:
-                    num += 1
-                    if num >= K:
-                        return turn
+    # 3. 올리는 칸에 올리기
+    if belt[0]:
+        robot[0] = 1
+        belt[0] -= 1
 
+    # 4. 내구도
+    if belt.count(0) >= K:
+        print(turn)
+        break
 
-N,K=map(int, input().split())
-a=list(map(int, (input().split())))
-
-# (내구도, 로봇여부) deque 생성
-belt = deque([[i,0] for i in a])
-turn = 0    # 벨트 회전 횟수 (= 단계)
-num = 0     # 내구도 0인 것의 개수
-
-print(solve())
+    turn += 1
