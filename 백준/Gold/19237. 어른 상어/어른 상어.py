a1cb80e8bb2d84f148d08f86248cf31f72ca85ab
haeseul <58204031+haeseul@users.noreply.github.com>
2024-04-04 21:50:49 +0900

    [Gold II] Title: 어른 상어, Time: 280 ms, Memory: 112880 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.py
+++ 백준/Gold/19237. 어른 상어/어른 상어.py
@@ -1,122 +1,108 @@
-def reduce_smell():
-    # 현재 상어 위치가 아니면서 냄새가 1보다 클 때만 갱신
-    sharks_loc = [v for v in shark.values()]
-    for i in range(N):
-        for j in range(N):
-            if not arr[i][j]: continue
-            if (i,j) in sharks_loc: continue
-            n, smell = arr[i][j].pop()
-            if smell > 1:
-                arr[i][j].append((n, smell-1))
+def OOB(i,j): return not (0<=i<N and 0<=j<N)
 
 
-def get_nxt():
-    global death
-    new = [[row[:] for row in rows] for rows in arr]
-
-    # 각 상어 새롭게 배치
-    for num in range(1, M+1):
-        # 상어위치, 현재방향, 가능한 방향 받아오기
-        ci, cj = shark[num]
-        if (ci,cj) == (-1,-1) : continue
-        cur_d = shark_d[num]
-        d_lst = dr[num][cur_d]
-
-        for d in range(4):
-            nd = d_lst[d]
-            ni, nj = ci+dir[nd][0], cj+dir[nd][1]
-            # 경계, 빈 칸 아닌 곳 체크
-            if not (0<=ni<N and 0<=nj<N): continue
-            if arr[ni][nj]: continue
-
-            # 다른 상어가 있으면 내가 더 작을 때만 갱신
-            if new[ni][nj]:
-                other = new[ni][nj].pop()
-                if other[0] > num:
-                    new[ni][nj].append((num, K))
-                    shark[other[0]] = (-1,-1)
-                    shark[num] = (ni, nj)
-                    shark_d[num] = nd
-                else:   # 내가 더 크면 나 죽음
-                    new[ni][nj].append(other)
-                    shark[num] = (-1,-1)
-                    death += 1
-            # 없을 땐 그냥 갱신
-            else:
-                new[ni][nj].append((num, K))
-                shark[num] = (ni, nj)
-                shark_d[num] = nd
-            break
-
-        # 갈 수 있는 곳이 없다면 내 냄새 있는 곳으로
-        else:
-            for d in range(4):
-                nd = d_lst[d]
-                ni, nj = ci + dir[nd][0], cj + dir[nd][1]
-                # 경계, 다른 냄새 체크
-                if not (0<=ni<N and 0<=nj<N): continue
-                if arr[ni][nj][0][0] != num: continue
-
-                # 다른 상어가 있으면 나이거나 내가 더 작을 때만 갱신
-                if new[ni][nj]:
-                    other = new[ni][nj].pop()
-                    if other[0] >= num:
-                        new[ni][nj].append((num, K))
-                        shark[other[0]] = (-1, -1)
-                        shark[num] = (ni, nj)
-                        shark_d[num] = nd
-                    else:  # 내가 더 크면 나 죽음
-                        new[ni][nj].append(other)
-                        shark[num] = (-1, -1)
-                        death += 1
-                # 없을 땐 그냥 갱신
-                else:
-                    new[ni][nj].append((num, K))
-                    shark[num] = (ni, nj)
-                    shark_d[num] = nd
-                break
-
-    # 갱신된 상어 위치 리턴
-    return new
+# 전처리 ################################################################################
 
 
 N,M,K = map(int, input().split())
-dir = ((0,0),(-1,0),(1,0),(0,-1),(0,1))     # 1상 2하 3좌 4우
-arr = [[[] for _ in range(N)] for _ in range(N)]
-shark = dict()
-smells = []
-death = 0
+arr = [[(0,0) for _ in range(N)] for _ in range(N)]
 
+# { 상어번호 : 상어위치 }
+shark_loc = dict()
 for i in range(N):
     a = list(map(int, input().split()))
     for j in range(N):
         if not a[j]: continue
-        shark[a[j]] = (i,j)
+        # 처음 - 모든 상어가 자신 위치에 자신의 냄새 (K) 뿌림
+        arr[i][j] = (a[j], K)
+        shark_loc[a[j]] = (i,j)
 
-# 각 상어의 시작방향
+# 기본 : 1상 2하 3좌 4우
+dir = ((0,0),(-1,0),(1,0),(0,-1),(0,1))
+
+# { 상어번호 : 현재 상어 방향 }
 shark_d = dict()
 a = list(map(int, input().split()))
-for i in range(1, M+1):
-    shark_d[i] = a[i-1]
 
-# arr에 (상어, 남은시간) 배치
-for num, loc in shark.items():
-    si,sj = loc
-    arr[si][sj].append((num, K))
-
-# M마리 만큼 4방향씩 반복
-dr = dict()
+# { 상어번호 : [각 방향에 따른 우선순위] }
+nxt_d = dict()
 for m in range(1, M+1):
-    dr[m] = dict()
-    for d in range(1, 5):
-        dr[m][d] = tuple(map(int, input().split()))
+    shark_d[m] = a[m-1]
+    nxt_d[m] = dict()
+    for dr in range(1,5):
+        nxt_d[m][dr] = list(map(int, input().split()))
 
-# 1000초까지 반복
-for time in range(1001):
-    if death == M-1:
-        print(time)
+
+#################################################################################
+
+
+for t in range(1001):
+
+    # 남은 상어 카운트
+    left = 0
+
+    # 1초마다 모든 상어 동시에 인접칸으로 이동 후 냄새 또 뿌림
+    new = [l[::] for l in arr]
+    new_loc = set()
+    for num in range(1, M+1):
+        si,sj = shark_loc[num]
+        if (si,sj) == (-1,-1): continue
+        left += 1
+
+        sd = shark_d[num]
+
+        # 이동방향 : 우선순위 따라 인접칸 중 아무 냄새 없는 칸으로
+        for d in range(4):
+            nd = nxt_d[num][sd][d]
+            ni, nj = si + dir[nd][0], sj + dir[nd][1]
+            # 경계 밖이거나 냄새있으면 다른 방향 탐색
+            if OOB(ni,nj) or arr[ni][nj][1]: continue
+            si,sj,sd = ni,nj,nd
+            break
+        # 없다면 내 냄새 있는 칸 방향으로
+        else:
+            for d in range(4):
+                nd = nxt_d[num][sd][d]
+                ni, nj = si + dir[nd][0], sj + dir[nd][1]
+                if OOB(ni, nj) or arr[ni][nj][0] != num: continue
+                si, sj, sd = ni, nj, nd
+                break
+
+        # 모든 상어 이동 후 한 칸에 여러 상어 남아있으면 가장 작은 번호 상어만 남음
+        if not new[si][sj][0]:
+            new[si][sj] = (num,K)
+            shark_loc[num] = (si,sj)
+            shark_d[num] = sd
+            new_loc.add((si,sj))
+        else:
+            now_shark = new[si][sj][0]
+            if now_shark >= num:
+                new[si][sj] = (num,K)
+                shark_loc[num] = (si, sj)
+                shark_d[num] = sd
+                new_loc.add((si, sj))
+
+                # 기존에 있던 상어 죽음
+                if now_shark != num:
+                    shark_loc[now_shark] = (-1,-1)
+            else:   # 나 죽음
+                shark_loc[num] = (-1,-1)
+    arr = new
+
+    # 한 마리만 남음!
+    if left == 1:
+        print(t)
         break
-    arr = get_nxt()
-    reduce_smell()
+
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j][1] and (i,j) not in new_loc:
+                smell = arr[i][j][1]
+                if smell-1 == 0:
+                    arr[i][j] = (0,0)
+                else:
+                    arr[i][j] = (arr[i][j][0], smell-1)
+
+# 1000초 넘어도 다른 상어 남아있으면 -1
 else:
     print(-1)
