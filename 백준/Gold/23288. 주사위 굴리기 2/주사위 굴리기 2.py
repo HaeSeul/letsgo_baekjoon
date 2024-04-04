c26fb8cb9a30e49398d5c301517b54fadbf479c9
haeseul <58204031+haeseul@users.noreply.github.com>
2024-04-04 22:54:04 +0900

    [Gold III] Title: 주사위 굴리기 2, Time: 224 ms, Memory: 114220 KB -BaekjoonHub

--- 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
+++ 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
@@ -1,56 +1,68 @@
 from collections import deque
 
-def bfs(i,j):
+def OOB(i,j): return not (0<=i<N and 0<=j<M)
+
+def bfs(i,j,B):
     v = [[0]*M for _ in range(N)]
     v[i][j] = 1
     q = deque([(i,j)])
-    C = 1   # 나 포함 세기
+    C = 1
     while q:
         ci,cj = q.popleft()
-        for di, dj in dir:
+        for di,dj in dir:
             ni,nj = ci+di, cj+dj
-            # 범위, 방문, 나랑 같은 값인지 체크
-            if not (0 <= ni < N and 0 <= nj < M): continue
+            if OOB(ni,nj): continue
             if v[ni][nj]: continue
-            if arr[ni][nj] != arr[i][j]: continue
+            if arr[ni][nj] != B: continue
             C += 1
             v[ni][nj] = 1
             q.append((ni,nj))
     return C
 
+dir = ((0,1),(1,0),(0,-1),(-1,0))
 
 N,M,K = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 score = 0
 
-dir = ((0,1),(1,0),(0,-1),(-1,0))   # 우0 하1 좌2 상3
-idx = {0:[0,4,2,5,3,1], 1:[3,0,1,2,4,5], 2:[0,5,2,4,1,3], 3:[1,2,3,0,4,5]}
+# 주사위 오른쪽부터 이동
+i,j = 0,0
 d = 0
-ci,cj = 0,0
 dice = [2,1,5,6,4,3]
 
-for k in range(K):
-    ni,nj = ci+dir[d][0], cj+dir[d][1]
-
-    # 범위 밖이면 반대로 한 칸 굴러감
-    if not (0<=ni<N and 0<=nj<M):
+for _ in range(K):
+    B,U,F,D,L,R = dice[0],dice[1],dice[2],dice[3],dice[4],dice[5]
+    # 현재 방향으로 주사위 한 칸 굴러감
+    ni,nj = i+dir[d][0], j+dir[d][1]
+    # 그 방향에 칸이 없으면
+    if OOB(ni,nj):
+        # 방향 반대로 바꿔서 다음 한 칸 굴러감
         d = (d+2)%4
-        ni, nj = ci + dir[d][0], cj + dir[d][1]
+        ni,nj = i+dir[d][0], j+dir[d][1]
+    i,j = ni,nj
+    if d==0:
+        U,D,L,R = L,R,D,U
+    elif d==1:
+        B,U,F,D = D,B,U,F
+    elif d==2:
+        U,D,L,R = R,L,U,D
+    else:
+        B,U,F,D = U,F,D,B
+    dice = [B,U,F,D,L,R]
 
-    tmp = [0] * 6
-    for i in range(6):
-        tmp[i] = dice[idx[d][i]]
-    dice = tmp
+    # 도착칸 점수 획득 = B * C
+    B = arr[i][j]
+    # 현재 칸 숫자B와 같은 수 연속되는 개수 C
+    C = bfs(i,j,B)
+    score += B*C
 
-    # 도착 칸 점수 획득
-    B = arr[ni][nj]
-    C = bfs(ni,nj)
-    score += B * C
-
-    # 방향 바꾸기
+    # 주사위 아랫면 숫자A와 칸에 있는 숫자B 비교해 방향 결정
     A = dice[3]
-    if A > B:   d = (d+1)%4
-    elif A < B: d = (d-1)%4
 
-    ci,cj = ni,nj
+    # A > B : 시계90
+    if A > B : d = (d+1)%4
+    # A < B : 반시계90
+    elif A < B : d = (d-1)%4
+
+# 각 이동에서 획득하는 점수 합
 print(score)
