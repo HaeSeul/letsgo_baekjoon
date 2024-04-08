583dda2d81ae660b7fdce5be715e210f819497e9
haeseul <58204031+haeseul@users.noreply.github.com>
2024-04-08 15:43:18 +0900

    [Gold I] Title: 마법사 상어와 복제, Time: 712 ms, Memory: 224168 KB -BaekjoonHub

--- 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
+++ 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
@@ -1,123 +1,122 @@
-arrow = {0:'←', 1:'↖', 2:'↑', 3:'↗', 4:'→', 5:'↘', 6:'↓', 7:'↙'}
+def OOB(i,j): return not (0<=i<N and 0<=j<N)
+
+def make_order():
+    # 000 001 002 003 ...
+    order = []
+    for i in range(4):
+        for j in range(4):
+            for k in range(4):
+                order.append((i,j,k))
+    return order
+
+def move(arr):
+    move = [[list() for _ in range(N)] for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if not arr[i][j]: continue
+
+            # 여러 마리 있을 수 있음
+            while arr[i][j]:
+                fd = arrow_idx[arr[i][j].pop()]
+
+                # 상어칸, 물고기냄새칸, 격자밖 불가능
+                fi,fj,nd = i,j,fd
+                for _ in range(8):
+                    ni,nj = fi+dir[nd][0], fj+dir[nd][1]
+                    # 이동 가능한 칸 향할 때까지 45 반시계 회전 후 이동 (없으면 안 감)
+                    if OOB(ni,nj) or (ni,nj)==(si,sj) or smell[ni][nj]:
+                        nd = (nd-1)%8
+                    else:
+                        fi,fj,fd = ni,nj,nd
+                        break
+                move[fi][fj].append(arrow[fd])
+
+    return move
+
+
+#################################################################################################
+
+arrow = {0:'←', 1:'↖', 2:'↑', 3:'↗', 4:'→', 5:'↘', 6:'↓', 7:'↙' }
 arrow_idx = {v:k for k,v in arrow.items()}
-
-def dfs(op, n, i, j, cnt, v):  # 움직일 방향, n번 이동, 상어위치, 물고기개수
-    global mx_fish
-    if n == 3:  # 3번 이동하면 끝
-        mx_fish.append((cnt, op))  # 물고기개수, 사전순 방향
-        return
-
-    sd = op[n]  # 움직일 방향
-    ni, nj = i + dir_s[sd][0], j + dir_s[sd][1]
-    # 범위 밖이라면 불가능
-    if not (0 <= ni < 4 and 0 <= nj < 4): return
-
-    if v[ni][nj]:  # 방문했던 곳이면 카운트 X (중복)
-        dfs(op, n + 1, ni, nj, cnt, v)
-    else:
-        v[ni][nj] = 1
-        dfs(op, n + 1, ni, nj, cnt + len(arr[ni][nj]), v)
-
-
-arr = [[list() for _ in range(4)] for _ in range(4)]
-smell = [[0 for _ in range(4)] for _ in range(4)]
 dir = ((0,-1),(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1))
-dir_s = ((0,0), (-1,0), (0,-1), (1,0), (0,1))
-M, S = map(int, input().split())
+dir4 = ((-1,0),(0,-1),(1,0),(0,1))  # 위 왼 아래 오
 
+N = 4
+M, S = map(int, input().split())
+arr = [[list() for _ in range(N)] for _ in range(N)]
 for _ in range(M):
-    x,y,D = map(int, input().split())
-    arr[x-1][y-1].append(arrow[D-1])
+    x,y,d = map(lambda x:x-1, map(int, input().split()))
+    arr[x][y].append(arrow[d])
 si,sj = map(lambda x:x-1, map(int, input().split()))
 
+smell = [[0]*N for _ in range(N)]
+order = make_order()
 
-# 사전순 이동방법 만들기
-'''
-    111 112 113 114 121 122 ... 444 (64개)
-숫자, 잡은 물고기 수 순으로 정렬
-'''
-order = []
-for i in range(4):
-    tmp = []
-    for j in range(4):
-        for k in range(4):
-            tmp.append([i + 1, j + 1, k + 1])
-    order += tmp
 
+##################################################################################################
 
 for _ in range(S):
-    ### 복제할 물고기 저장
-    fish = []
-    for i in range(4):
-        for j in range(4):
-            if not arr[i][j]: continue
-            for k in range(len(arr[i][j])):
-                fish.append((i,j,arrow_idx[arr[i][j][k]]))
+    # [ 1 ] 모든 물고기 복제
+    now_fish = [[col[:] for col in row] for row in arr]
 
+    # [ 2 ] 모든 물고기 한 칸 이동 (인접8방)
+    arr = move(arr)
 
-    ### 모든 칸의 물고기 한 칸 이동
-    move = [[list() for _ in range(4)] for _ in range(4)]
-    for i in range(4):
-        for j in range(4):
-            if not arr[i][j]: continue
+    # [ 3 ] 상어 연속 3칸 이동 (인접4방)
+    mx, mxi, mxj, mx_copy, mx_smell = -1, 0, 0, [], []
+    for op in order:
+        copy = [[col[:] for col in row] for row in arr]
+        smell_c = [l[:] for l in smell]
+        catch = 0
+        possible = True
+        tmp = []
 
-            # 한 칸에 물고기 여러 마리 있을 수 있음
-            while arr[i][j]:
-                fi,fj = i,j
-                dr = arr[i][j].pop()
-                dr = arrow_idx[dr]
-                for d in range(dr, dr-8, -1):   # 반시계 회전
-                    d %= 8
-                    ni,nj = fi+dir[d][0], fj+dir[d][1]
+        ni,nj = si,sj
+        for d in op:
+            ni,nj = ni+dir4[d][0], nj+dir4[d][1]
 
-                    # 격자 밖, 상어, 물고기냄새 체크
-                    if not (0<=ni<4 and 0<=nj<4): continue
-                    if ni==si and nj==sj: continue
-                    if smell[ni][nj]: continue
+            # 격자 범위 넘어가면 불가능한 방법
+            if OOB(ni,nj):
+                possible = False
+                break
 
-                    # 이동 가능하면 좌표 갱신
-                    fi,fj,dr = ni,nj,d
-                    break
-                move[fi][fj].append(arrow[dr])
-    arr = move
+            tmp.append((ni,nj))
+            if not copy[ni][nj]: continue
 
+            # 이동하는 중에 물고기 있는 칸이면 그 칸 모든 물고기 제외 -> 냄새 남김
+            catch += len(copy[ni][nj])
+            copy[ni][nj] = []
+            smell_c[ni][nj] = 3
 
-    ### 상어 이동 전에 냄새 -1씩
-    for i in range(4):
-        for j in range(4):
+        # 중간에 안 끊기고 3번 이동 성공했다면 max 비교
+        else:
+            # 사전순으로 가장 앞서는 방법 이용
+            if mx < catch:
+                mx = catch
+                mx_copy = copy
+                mx_smell = smell_c
+                mxi, mxj = ni,nj
+
+        if not possible: continue
+
+    # 가능한 방법 중 제외되는 물고기 가장 많은 방법으로 이동
+    arr = mx_copy
+    smell = mx_smell
+    si, sj = mxi, mxj
+
+    # [ 4 ] 2턴 전 물고기 냄새 사라짐
+    for i in range(N):
+        for j in range(N):
             if smell[i][j]: smell[i][j] -= 1
 
 
-    ### 상어가 갈 수 있는 곳과 잡을 수 있는 물고기 개수 구하기
-    mx_fish = []
+    # [ 5 ] 1번에서의 물고기 복제
+    for i in range(N):
+        for j in range(N):
+            if now_fish[i][j]: arr[i][j] += now_fish[i][j]
 
-    for op in order:
-        v = [[0]*4 for _ in range(4)]
-        dfs(op, 0, si, sj, 0, v)
-
-    # 물고기 잡은 개수 큰 순, 사전순 정렬
-    mx_fish.sort(key = lambda x:(-x[0], x[1]))
-
-    # 상어 3번 이동
-    nxt_move = mx_fish[0][1]
-    for nxt_d in nxt_move:
-        si,sj = si+dir_s[nxt_d][0], sj+dir_s[nxt_d][1]
-        # 상어가 이동하는 도중에 물고기가 있다면 없애고 냄새 뿌리기
-        if arr[si][sj]:
-            arr[si][sj] = []
-            smell[si][sj] = 2
-
-
-    # 물고기 복제
-    # print(fish)
-    for fi,fj,fd in fish:
-        arr[fi][fj].append(arrow[fd])
-
-
-# 물고기 수 카운트
-ans = 0
-for i in range(4):
-    for j in range(4):
-        if arr[i][j]:
-            ans += len(arr[i][j])
-print(ans)
+cnt = 0
+for i in range(N):
+    for j in range(N):
+        if arr[i][j]: cnt += len(arr[i][j])
+print(cnt)
